f377b601eed3462262d1ed34c444f55f91901763
Mohamed <mo7ammed.sabre@gmail.com>
2024-04-18 15:46:58 +0200

    init PASS UAE

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Hero from './Components/Hero/Hero';
+import Test from './Components/Test/Test';
+
+function App() {
+  return <>
+    <Hero />
+    <Test />
+  </>
+}
+
+export default App;
